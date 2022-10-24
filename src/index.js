7f241783bb26f887fbc359c41c6947a0aa14cedd
IvannaKhomik <ivanna.khomik@gmail.com>
2022-10-24 13:16:28 +0300

    changes for App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./components/App";
-import { GlobalStyle } from "./GlobalStyles.styled";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './components/App';
+import { GlobalStyle } from './GlobalStyles.styled';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
